516cbf5a2278d720c53b9b8d5e9c0da81c47ddc3
Michael Gorbunov <mg0rby@yandex.ru>
2024-07-16 14:01:23 +0300

    сохранение файлов с указанием дата время

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import os
 from src.file_operation import JSONData
 from config import DATA_DIR
+from src.get_data_api import HeadHunterAPI
+from datetime import datetime
 
-file_name = input("Введите имя файла в директории дата")
-file_path = os.path.join(DATA_DIR, file_name)
+save_file_name=datetime.now().strftime('%Y_%m_%d-%H_%M')
 
-data1=JSONData.load_file(JSONData(file_path))
+# file_name = input("Введите имя файла в директории дата")
+# file_path = os.path.join(DATA_DIR, file_name)
+#
+# data1=JSONData.load_file(JSONData(file_path))
+# test_list = []
+# print(len(data1["items"]))
+# for item in data1["items"]:
+#     test_list.append(item)
+#     print(item)
+#     print(item["area"]["name"])#city
+# print(type(data1))
+#
+# data2=JSONData(os.path.join(DATA_DIR, "my_save.json"))
+# data2.write_file(test_list)
+
+
+
+search_vacancy = input('Введите запрос:\n')
+data1=HeadHunterAPI.get_vacancies(HeadHunterAPI(search_vacancy))
+
 test_list = []
 print(len(data1["items"]))
 for item in data1["items"]:
@@ -14,5 +34,23 @@ for item in data1["items"]:
     print(item["area"]["name"])#city
 print(type(data1))
 
-data2=JSONData(os.path.join(DATA_DIR, "my_save.json"))
+data2=JSONData(os.path.join(DATA_DIR, save_file_name+"_"+search_vacancy+".json"))
 data2.write_file(test_list)
+
+
+#
+# for fname in os.listdir(DATA_DIR):
+#     print(fname)
+#     path = os.path.join(DATA_DIR, fname)
+#     if os.path.isdir(DATA_DIR):
+#         # skip directories
+#         continue
+#     print(fname)
+
+
+# for entry in os.scandir(DATA_DIR):#вывод списка файлов
+#     if entry.is_dir():
+#         # skip directories
+#         continue
+#     else:
+#         print(entry.name)
--- src/get_data_api.py
+++ src/get_data_api.py
@@ -1,2 +1,22 @@
 #https://api.hh.ru/vacancies?currency=RUR&only_with_salary=true&area=113&per_page=100&text=разработчик
 #https://api.hh.ru/openapi/redoc#tag/Poisk-vakansij/operation/get-vacancies
+
+import requests
+class HeadHunterAPI():
+    """Работа с API платформы HeadHunter"""
+    def __init__(self, keyword: str):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.params = {
+            'text': keyword,
+            'area': 113,
+            'only_with_salary': True,
+            'page': 0,
+            'per_page': 100,
+            }
+
+    def get_vacancies(self):
+        """Возвращает вакансии по заданному параметру"""
+
+        response = requests.get(self.url, params=self.params)
+        # return response.json()['items']
+        return response.json()
