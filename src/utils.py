a7c534d7f07c63a5642c0144e3af31669e87e5ca
Michael Gorbunov <mg0rby@yandex.ru>
2024-07-19 11:29:47 +0300

    функция вывода списка файлов

--- src/utils.py
+++ src/utils.py
@@ -9,8 +9,11 @@ from datetime import datetime
 def clear_vacans():
     """загрузка файла и выделение отдельных атрибутов с сохранением в json"""
     save_file_name = datetime.now().strftime('%Y_%m_%d-%H_%M')
-
-    file_name = input("Введите имя файла в директории дата \n")
+    print("Список файлов в папке DATA")
+    m=json_file_list()
+    # file_name = input("Введите имя файла в директории дата \n")
+    file_index = int(input("Введите индекс файла из списка \n"))
+    file_name=m[file_index]
     file_path = os.path.join(DATA_DIR, file_name)
     basename, extension = os.path.splitext(file_name)
     prof=basename.split("_")[-1]
@@ -35,4 +38,24 @@ def clear_vacans():
     with open(os.path.join(DATA_DIR, save_file_name+"_"+prof+".vac"), 'w', encoding='utf-8') as f:
         json.dump(test_list, f, ensure_ascii=False, indent=4)
 
+# clear_vacans()
+
+def json_file_list():
+    """функция вывода списка файлов JSON"""
+    file_list= []
+    for entry in os.scandir(DATA_DIR):#вывод списка файлов
+        if entry.is_dir():
+            # skip directories
+            continue
+        elif entry.name.split(".")[-1] != "json":
+            continue
+        else:
+            # print(entry.name)
+            file_list.append(entry.name)
+
+    for i, name_file in enumerate(file_list):
+        print(i, name_file)#вывод списка файлов JSON
+    return file_list
+
+# json_file_list()
 clear_vacans()
