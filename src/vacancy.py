cc789ed81b0e034ec78dc35bf75eb77b823ee09d
Michael Gorbunov <mg0rby@yandex.ru>
2024-07-22 13:34:04 +0300

    класс для работы с вакансиями заготовка

---
+++ src/vacancy.py
@@ -0,0 +1,37 @@
+class Vacancy:
+    """
+    Класс для работы с вакансиями
+    """
+    vac_list = []
+    def __init__(self,id:int, name: str, city: str, salary_from: int, salary_to: int|None, url: str, requirement: str,responsibility:str):
+        self.id = id
+        self.name = name
+        self.city = city
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.url = url
+        self.requirement = requirement
+        self.responsibility = responsibility
+        Vacancy.vac_list.append(id)
+
+    def __str__(self):
+        return (f"{self.name}\n"
+                f"Id: {self.id}\n"
+                f"Город: {self.city}\n"
+                f"Зарплата от: {self.salary_from } \n"
+                f"Зарплата до: {self.salary_to if self.salary_to else "Не указана"} \n"
+                f"Ссылка: {self.url}\n"
+                f"Требования: {self.requirement}\n"
+                f"Обязанности: {self.responsibility}\n")
+
+
+
+
+vac1=Vacancy(10,"name1","Город Н",100,None,"https://gg.com","Уметь всё","Жить на работе")
+vac2=Vacancy(11,"name2","Город F",105,120,"https://gg.com","Уметь всё","Жить на работе")
+vac3=Vacancy(12,"name3","Город V",110,130,"https://gg.com","Уметь всё","Жить на работе")
+vac4=Vacancy(13,"name4","Город Z",120,150,"https://gg.com","Уметь всё","Жить на работе")
+
+print(vac1,vac2,vac3,vac4)
+print(Vacancy.vac_list)
+
