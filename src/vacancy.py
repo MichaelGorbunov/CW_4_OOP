255804e2c6eb8574dee3fcbff9c9f6b438295ec0
Michael Gorbunov <mg0rby@yandex.ru>
2024-07-24 10:34:34 +0300

    создан метод проверки

--- src/vacancy.py
+++ src/vacancy.py
@@ -9,15 +9,15 @@ class Vacancy:
     vac_list: list = []
 
     def __init__(
-        self,
-        id: int,
-        name: str,
-        city: str,
-        salary_from: int,
-        salary_to: int,
-        url: str,
-        requirement: str,
-        responsibility: str,
+            self,
+            id: int,
+            name: str,
+            city: str,
+            salary_from: int,
+            salary_to: int,
+            url: str,
+            requirement: str,
+            responsibility: str,
     ):
         self.id = id
         self.name = name
@@ -38,7 +38,7 @@ class Vacancy:
             f"{self.name}\n"
             f"Id: {self.id}\n"
             f"Город: {self.city}\n"
-            f"Зарплата от: {self.salary_from} \n"
+            f"Зарплата от: {self.salary_from if self.salary_from else "Не указана"} \n"
             f"Зарплата до: {self.salary_to if self.salary_to else "Не указана"} \n"
             f"Ссылка: {self.url}\n"
             f"Требования: {self.requirement}\n"
@@ -81,37 +81,71 @@ class Vacancy:
             print("Вакансий с такими критериями не найден")
         return filtered_list
 
-
-vac1 = Vacancy(
-    10, "name1", "Город Н", 100, None, "https://gg.com", "Уметь всё ", "Жить на работе"
-)
-vac2 = Vacancy(
-    11, "name2", "Город F", None, 120, "https://gg.com", "Знать всё", "Жить на работе"
-)
-vac3 = Vacancy(
-    12, "name3", "Город V", 110, 130, "https://gg.com", "Рулить всем", "Жить на работе"
-)
-vac4 = Vacancy(
-    13,
-    "name4",
-    "Город Z",
-    120,
-    150,
-    "https://gg.com",
-    "Ездить на всём",
-    "Жить на работе",
-)
-
-filter_words=["уметь","знать"]
-# filter_words=["супермен,мегамозг"]
-
-print("**********************************************")
-vac_list=Vacancy.vac_list
-filtered_vac=Vacancy.filter_vacancies(vac_list,filter_words)
-for item in filtered_vac:
-    print(item)
+    def range_from_salary(self, salary_from, salary_to) -> bool:
+        """Метод проверки объекта-вакансии перекрытия зарплатной вилки
+        salary_from:int зарплата от
+        salary_to:int зарплата до
+        возвращает bool
+        """
+        if isinstance(salary_from, int) is True and isinstance(salary_to, int) is True:
+            if self.salary_from >= salary_from and self.salary_to >= salary_to:
+                return True
+            else:
+                return False
+                # return self
 
 
+def func1():
+    vac1 = Vacancy(
+        10, "name1", "Город Н", 100, None, "https://gg.com", "Уметь всё ", "Жить на работе"
+    )
+    vac2 = Vacancy(
+        11, "name2", "Город F", None, 120, "https://gg.com", "Знать всё", "Жить на работе"
+    )
+    vac3 = Vacancy(
+        12, "name3", "Город V", 110, 130, "https://gg.com", "Рулить всем", "Жить на работе"
+    )
+    vac4 = Vacancy(
+        13,
+        "name4",
+        "Город Z",
+        120,
+        150,
+        "https://gg.com",
+        "Ездить на всём",
+        "Жить на работе",
+    )
+
+
+def func2():
+    filter_words = ["уметь", "знать"]
+    # filter_words=["супермен,мегамозг"]
+
+    print("**********************************************")
+    vac_list = Vacancy.vac_list
+    filtered_vac = Vacancy.filter_vacancies(vac_list, filter_words)
+    for item in filtered_vac:
+        print(item)
+
+
+func1()
+
+
+# func2()
+
+def func3():
+    vac_list = Vacancy.vac_list
+    sort_list = []
+    for item in vac_list:
+        if Vacancy.range_from_salary(item, 0, 0) is True:
+            sort_list.append(item)
+            print(item)
+    print(sort_list)
+    # print(Vacancy.vac_list)
+    # print(Vacancy.vac_list[1].salary_to)
+
+
+func3()
 
 # # print(vac1,vac2,vac3,vac4)
 # Vacancy.vacancies_sort_salary()
